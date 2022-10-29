7a5e87f6911d714e2821c94ef3bacdb7b8996f50
Marshalng <36773702+Marshalng@users.noreply.github.com>
2022-10-29 22:49:00 +0100

    Update stage_one.php

--- stage_one.php
+++ stage_one.php
@@ -1,9 +1,9 @@
 <?php
- 
-    $slackUsername = (string) "Marshal";
-    $backend = (boolean) true;
+    header('Content-type: application/json');
+    $slackUsername = "Marshal";
+    $backend = true;
     $age = (int) 26;
-    $bio = (string) "My name is Asubiojo Olujuwon, a backend PHP  developer with 3 years industrial experience in building  applications." ;
+    $bio = "My name is Asubiojo Olujuwon, a backend PHP  developer with 3 years industrial experience in building  applications." ;
 
 
     $res = json_encode([
