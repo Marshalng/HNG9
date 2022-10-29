4610a78db55f46506aefcd3cf7ff0ce4ce561692
Marshalng <36773702+Marshalng@users.noreply.github.com>
2022-10-29 19:49:59 +0100

    intitial commit

---
+++ stage_one.php
@@ -0,0 +1,22 @@
+<?php
+ if (isset($_GET)){
+    $slackUsername = (string) "Marshal";
+    $backend = (boolean) true;
+    $age = (int) 26;
+    $bio = (string) "My name is Asubiojo Olujuwon, a backend PHP  developer with 3 years industrial experience in building  applications." ;
+
+
+    $res = json_encode([
+        'slackUsername' => $slackUsername,
+        'backend' => $backend,
+        'age' => $age,
+        'bio' => $bio,
+    ]);
+        echo $res;
+
+ } 
+
+?>
+
+
+
